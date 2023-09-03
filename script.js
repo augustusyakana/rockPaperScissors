61b4d293cb75219f8e01dafd6da73cc2f3139561
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2023-09-02 16:51:11 -1000

    added UI to game

--- script.js
+++ script.js
@@ -1,58 +1,86 @@
-let userScore = 0;
+let playerScore = 0;
 let computerScore = 0;
-
 let draws = 0;
 
-console.log("First Player to Reach 5points \nor Best out of 10 rounds \nwins the game!! GO!!");
+const buttons = document.querySelectorAll('input');
 
-const choices = ["rock", "paper", "scissors"];
-function getComputerChoice(arr) {
-    return (arr[(Math.floor(Math.random() * arr.length))]);
+
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    return choices[(Math.floor(Math.random() * choices.length))];
 }
 
-function game() {
-    for (let i = 1; i <= 10; i++) {
-        let userSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-        const computerSelection = getComputerChoice(choices);
-        let roundResult = playRound(userSelection, computerSelection);
-        console.log(roundResult);
-        
-        if (userScore === 5 || computerScore === 5) {
-            break;
+// add code to disable buttons when score reaches 5
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true;
+    })
+}
+
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    let result = '';
+    let computerScoreResults = '';
+    let playerScoreResults = '';
+    let drawsResult = '';
+    let gameOver = '';
+
+    // when the player wins the round
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        playerScore += 1;
+
+        result = `You won this round! ${playerSelection} beats ${computerSelection}`;
+        playerScoreResults = `Player Score: ${playerScore}`;
+        computerScoreResults = `Computer Score: ${computerScore}`;
+        drawsResult = `Draws: ${draws}`;
+
+        if (playerScore == 5) {
+            playerScoreResults = `Player Score: ${playerScore}`;
+            computerScoreResults = `Computer Score: ${computerScore}`;
+            drawsResult = `Draws: ${draws}`;
+
+            gameOver = `I'll get you next time! <br>Refresh the page to play again`;
+            disableButtons();
         }
 
-    }
-}
+    } else if (playerSelection == computerSelection) {
+        draws += 1;
+        result = `Draw! You both chose ${playerSelection}`;
+        playerScoreResults = `Player Score: ${playerScore}`;
+        computerScoreResults = `Computer Score: ${computerScore}`;
+        drawsResult = `Draws: ${draws}`;
 
-// let theButton = document.getElementsByClassName("button");
-// theButton.addEventListener("click", game());
-
-function playRound(user, computer) {
-    if (user === computer) {
-        draws++;
-        let draw = `Draw, you both chose ${user}!`;
-        return draw;
-    } else if (user === "rock" && computer === "scissors") {
-        userScore++;
-        let userWinRound = `You Win this round, ${user} beats ${computer}!`
-        return userWinRound;
-    } else if (user === "scissors" && computer === "paper") {
-        userScore++
-        let userWinRound = `You Win this round, ${user} beats ${computer}!`
-        return userWinRound;
-    } else if (user === "paper" && computer === "rock") {
-        userScore++
-        let userWinRound = `You Win this round, ${user} beats ${computer}!`
-        return userWinRound;
     } else {
-        computerScore++;
-        let computerWinRound = `You lost, ${computer} beats ${user}!`
-        return computerWinRound;
+        computerScore += 1;
+        result = `You lost this round! ${computerSelection} beats ${playerSelection}`;
+        playerScoreResults = `Player Score: ${playerScore}`;
+        computerScoreResults = `Computer Score: ${computerScore}`;
+        drawsResult = `Draws: ${draws}`;
+
+        if (computerScore == 5) {
+
+            playerScoreResults = `Player Score: ${playerScore}`;
+            computerScoreResults = `Computer Score: ${computerScore}`;
+            drawsResult = `Draws: ${draws}`;
+            gameOver = `Loser! <br>Refresh the page to play again!`
+            disableButtons();
+
+        }
     }
+
+    document.getElementById('result').innerHTML = result;
+    document.getElementById('playerScore').innerHTML = playerScoreResults;
+    document.getElementById('computerScore').innerHTML = computerScoreResults;
+    document.getElementById('draws').innerHTML = drawsResult;
+    document.getElementById('gameOver').innerHTML = gameOver;
+    return;
+
 }
 
-game()
-console.log(`\nGAME OVER! See Scores Below:`)
-console.log(`Computer Score: ${computerScore}`);
-console.log(`User Score: ${userScore}`);
-console.log(`Draws: ${draws}`);
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        playRound(button.value.toLowerCase());
+    })
+})
